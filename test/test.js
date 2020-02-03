7641fd6a0be8af90625aceebbdb031c550eccd0e
Louis Chan <louischan@oursky.com>
2020-02-03 11:48:51 +0800

    Patch eslint results so that paths are relative

--- test/test.js
+++ test/test.js
@@ -2,12 +2,24 @@ const path = require("path");
 
 const { CLIEngine } = require("eslint");
 
+function patchResults(results) {
+  const cwd = process.cwd();
+  return results.map(result => {
+    return {
+      ...result,
+      filePath: path.relative(cwd, result.filePath),
+    };
+  });
+}
+
 function runTest(filename) {
   test(filename, () => {
     const cli = new CLIEngine();
     const report = cli.executeOnFiles([path.join("src", filename)]);
     const formatter = cli.getFormatter("unix");
-    const actual = formatter(report.results);
+    const results = patchResults(report.results);
+    // Patch results so that all paths are relative to cwd.
+    const actual = formatter(results);
     expect(actual).toMatchSnapshot();
   });
 }
