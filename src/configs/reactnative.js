755b6955c4f6b3e0fcf4c7f6a0b59df81c2ab231
Louis Chan <louischan@oursky.com>
2020-05-22 16:47:55 +0800

    Consider all react native rules

--- src/configs/reactnative.js
+++ src/configs/reactnative.js
@@ -1,6 +1,12 @@
 module.exports = {
   plugins: ["react-native"],
   rules: {
+    "react-native/no-color-literals": "off",
+    "react-native/no-inline-styles": "error",
     "react-native/no-unused-styles": "error",
+    "react-native/no-raw-text": "error",
+    "react-native/no-single-element-style-arrays": "error",
+    "react-native/sort-styles": "off",
+    "react-native/split-platform-components": "off",
   },
 };
