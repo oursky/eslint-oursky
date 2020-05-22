2e57aaae212adb1511069e40ae80679d5157f9b9
Louis Chan <louischan@oursky.com>
2020-05-22 16:44:54 +0800

    Consider all react rules

--- src/configs/react.js
+++ src/configs/react.js
@@ -1,10 +1,10 @@
 module.exports = {
-  extends: ["plugin:react/recommended"],
   plugins: ["react"],
   settings: {
     react: {
       // <></> is available from 16.2
-      version: "16.2",
+      // Hooks is available from 16.8
+      version: "16.8",
     },
     linkComponents: [
       {
@@ -14,12 +14,92 @@ module.exports = {
     ],
   },
   rules: {
-    // react
-    // customize some recommended rules
+    "react/boolean-prop-naming": "off",
+    "react/button-has-type": "error",
+    "react/default-props-match-prop-types": "off",
+    "react/destructuring-assignment": "off",
+    "react/forbid-component-props": "off",
+    "react/forbid-dom-props": "off",
+    "react/forbid-elements": "off",
+    "react/forbid-foreign-prop-types": "off",
+    "react/forbid-prop-types": "off",
+    "react/function-component-definition": "off",
+    "react/jsx-child-element-spacing": "off",
+    "react/jsx-closing-bracket-location": "off",
+    "react/jsx-closing-tag-location": "off",
+    "react/jsx-curly-brace-presence": "off",
+    "react/jsx-curly-newline": "off",
+    "react/jsx-curly-spacing": "off",
+    "react/jsx-equals-spacing": "off",
+    "react/jsx-filename-extension": "off",
+    "react/jsx-first-prop-new-line": "off",
+    "react/jsx-handler-names": "off",
+    "react/jsx-indent": "off",
+    "react/jsx-indent-props": "off",
+
+    "react/jsx-key": [
+      "error",
+      {
+        checkFragmentShorthand: true,
+      },
+    ],
+
+    "react/jsx-max-depth": "off",
+    "react/jsx-max-props-per-line": "off",
+
+    "react/jsx-no-bind": [
+      "error",
+      {
+        ignoreDOMComponents: true,
+        ignoreRefs: true,
+      },
+    ],
+
+    "react/jsx-no-comment-textnodes": "error",
+    "react/jsx-no-duplicate-props": "error",
+    "react/jsx-no-literals": "off",
+    "react/jsx-no-script-url": "error",
+    "react/jsx-no-target-blank": "error",
+    "react/jsx-no-useless-fragment": "off",
+    "react/jsx-one-expression-per-line": "off",
+    "react/jsx-pascal-case": "off",
+    "react/jsx-props-no-multi-spaces": "off",
+    "react/jsx-props-no-spreading": "off",
+    "react/jsx-sort-default-props": "off",
+    "react/jsx-sort-props": "off",
+    "react/jsx-tag-spacing": "off",
+    "react/jsx-uses-react": "error",
+    "react/jsx-uses-vars": "error",
+    "react/jsx-wrap-multilines": "off",
+    "react/no-adjacent-inline-elements": "error",
+    "react/no-array-index-key": "off",
+    "react/no-children-prop": "error",
+    "react/no-danger": "off",
+    "react/no-danger-with-children": "error",
+    "react/no-deprecated": "error",
+    "react/no-direct-mutation-state": "error",
+    "react/no-find-dom-node": "error",
+    "react/no-is-mounted": "error",
+    "react/no-multi-comp": "off",
+    "react/no-render-return-value": "error",
+    "react/no-set-state": "off",
+    "react/no-string-refs": "error",
+    "react/no-unknown-property": "error",
+    "react/no-unused-prop-types": "off",
+    "react/prefer-read-only-props": "off",
+    "react/react-in-jsx-scope": "error",
+    "react/require-default-props": "off",
+    "react/require-optimization": "off",
+    "react/require-render-return": "error",
+    "react/self-closing-comp": "off",
+    "react/sort-comp": "off",
+    "react/sort-prop-types": "off",
+    "react/state-in-constructor": "off",
+    "react/static-property-placement": "error",
+
     "react/display-name": "off",
-    "react/jsx-no-undef": "off",
+    "react/jsx-no-undef": "error",
     "react/prop-types": "off",
-    // not from recommended
     "react/no-access-state-in-setstate": "error",
     "react/no-did-mount-set-state": "error",
     "react/no-did-update-set-state": "error",
