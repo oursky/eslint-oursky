d26da3f65e1ed05968ae0fcbf7b26747e8ebcdf5
Louis Chan <louischan@oursky.com>
2022-08-24 16:22:45 +0800

    Avoid absolute in tests

--- test/test.js
+++ test/test.js
@@ -5,7 +5,15 @@ const { ESLint } = require("eslint");
 function runTest(filename) {
   test(filename, async () => {
     const cli = new ESLint();
+
     const lintResults = await cli.lintFiles([path.join("src", filename)]);
+    // Patch the path so that they are the same on any machine.
+    for (const r of lintResults) {
+      const absolute = r.filePath;
+      const relative = path.relative(process.cwd(), absolute);
+      r.filePath = relative;
+    }
+
     const formatter = await cli.loadFormatter("unix");
     const actual = await formatter.format(lintResults);
     expect(actual).toMatchSnapshot();
