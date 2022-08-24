2880764418cb9966d8c6b4e8a8f6453efca230c3
Louis Chan <louischan@oursky.com>
2022-08-24 14:10:40 +0800

    Update test infra

--- test/jest.config.js
+++ test/jest.config.js
@@ -1,5 +1,8 @@
 module.exports = {
-  // jest does not support ESM out of the box.
-  // https://github.com/typescript-eslint/typescript-eslint/issues/4210#issuecomment-981203332
-  resolver: "<rootDir>/jest-resolver.js",
+  preset: "ts-jest",
+  testEnvironment: "jsdom",
+  transform: {
+    "^.+\\.tsx?$": "ts-jest",
+  },
+  transformIgnorePatterns: ["<rootDir>/node_modules/"],
 };
