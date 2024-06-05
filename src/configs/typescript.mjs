d07c2350bbb87040f0eb70867d34ea111ff4bd30
YinYin Chiu <cychiuae@connect.ust.hk>
2024-06-05 18:41:37 +0800

    Disallow non null assertion by default

--- src/configs/typescript.mjs
+++ src/configs/typescript.mjs
@@ -83,7 +83,7 @@ export default {
     "@typescript-eslint/no-meaningless-void-operator": "error",
     "@typescript-eslint/no-non-null-asserted-nullish-coalescing": "error",
     "@typescript-eslint/no-non-null-asserted-optional-chain": "error",
-    "@typescript-eslint/no-non-null-assertion": "off",
+    "@typescript-eslint/no-non-null-assertion": "error",
     "@typescript-eslint/no-require-imports": "error",
     "@typescript-eslint/no-restricted-imports": "off",
     "@typescript-eslint/no-throw-literal": "error",
